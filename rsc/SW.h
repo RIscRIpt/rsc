1fabdcb9603e76cc23baf0d6d16d899d4a07443d
RIscRIpt <richard@riscript.com>
2018-07-13 21:34:06 +0300

    add SW class

---
+++ rsc/SW.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+
+namespace rsc {
+
+    class SW {
+    public:
+        SW(uint16_t value)
+            : value(value)
+        {}
+
+        uint16_t const value;
+
+        inline operator uint16_t() const noexcept { return value; }
+
+        inline uint8_t SW1() const noexcept { return value >> 8; }
+        inline uint8_t SW2() const noexcept { return value & 0xFF; }
+
+        inline uint8_t response_bytes_still_available() const noexcept { if (SW1() == 0x61) return SW2(); return 0; }
+    };
+
+}
