179d4f65f2cd5b0df1cf72adfe8b8feeabd2357f
RIscRIpt <richard@riscript.com>
2018-07-29 22:00:18 +0300

    Context: add operator SCARDCONTEXT

--- rsc/Context.h
+++ rsc/Context.h
@@ -9,7 +9,11 @@ namespace rsc {
         Context(DWORD dwScope);
         ~Context();
 
+        Context(Context const &other) = delete;
+        Context& operator=(Context const &rhs) = delete;
+
         inline SCARDCONTEXT handle() const noexcept { return hContext_; }
+        inline operator SCARDCONTEXT() const noexcept { return hContext_; }
 
     private:
         SCARDCONTEXT hContext_;
